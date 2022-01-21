243fec1947fad8078fd08e8e37be4e17f15b1af0
gist006 <1979420758@qq.com>
2022-01-21 17:34:44 +0800

    提交

--- src/components/standardTree/index.vue
+++ src/components/standardTree/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-tree checkable :tree-data="treeData" @check="handleSelect" :checkedKeys="checkKeyList" />
+  <a-tree checkable :tree-data="treeData" @check="handleSelect" v-model="checkKeyList" />
 </template>
 
 <script>
@@ -25,9 +25,42 @@ export default {
   created() {
     const data = this.filtrRoute(this.asyncRoutes);
     this.treeData = this.generateRoutes(data);
-    this.checkKeyList = this.role ? this.role.split(',') : [];
+    if (this.role) {
+      //比对
+      this.handleBatter(data);
+    } else {
+      this.checkKeyList = [];
+    }
   },
   methods: {
+    //比对
+    handleBatter(data) {
+      //先递归拿到所有路由子节点
+      const allRoute = this.getRouteId(data);
+      let roleRoute = this.role.split(',');
+      let newRoute = [];
+      allRoute.forEach(item => {
+        roleRoute.forEach(val => {
+          if (item == val) {
+            newRoute.push(val);
+          }
+        });
+      });
+      this.checkKeyList = newRoute;
+    },
+
+    getRouteId(routes) {
+      const res = [];
+      routes.forEach(item => {
+        if (item.children && item.children.length > 0) {
+          res.push(...this.getRouteId(item.children));
+        } else if (item.meta && item.meta.role) {
+          res.push(String(item.meta.role));
+        }
+      });
+      return res;
+    },
+
     filtrRoute(routes) {
       if (routes.length == 0) {
         return [];
@@ -52,9 +85,9 @@ export default {
       return res;
     },
 
-    handleSelect(selectedKeys) {
-      this.checkKeyList = selectedKeys;
-      this.$emit('changeTree', selectedKeys);
+    handleSelect(selectedKeys, e) {
+      let checkKeyList = [...selectedKeys, ...e.halfCheckedKeys];
+      this.$emit('changeTree', checkKeyList);
     }
   }
 };
