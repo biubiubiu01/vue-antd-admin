8d492ae0113f465cdbbff26054c2547c2a178f7f
gist006 <1979420758@qq.com>
2022-01-30 19:03:59 +0800

    修改cdn地址

--- src/plugins/cdn.js
+++ src/plugins/cdn.js
@@ -1,9 +1,9 @@
 const AMapCDN = 'https://webapi.amap.com/maps?v=1.3&key=73cddabc2173e0166a622f4483d3592a&plugin=AMap.DistrictSearch';
 const AMapUiCDN = 'https://webapi.amap.com/ui/1.0/main.js';
-const VueCDN = 'https://cdn.bootcdn.net/ajax/libs/vue/2.6.12/vue.min.js';
-const AxiosCDN = 'https://cdn.bootcdn.net/ajax/libs/axios/0.21.0/axios.min.js';
-const VueRouterCDN = 'https://cdn.bootcdn.net/ajax/libs/vue-router/3.2.0/vue-router.min.js';
-const VuexCDN = 'https://cdn.bootcdn.net/ajax/libs/vuex/3.5.1/vuex.min.js';
+const VueCDN = 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js';
+const AxiosCDN = 'https://cdn.jsdelivr.net/npm/axios@0.21.0/dist/axios.min.js';
+const VueRouterCDN = 'https://cdn.jsdelivr.net/npm//vue-router@3.2.0/dist/vue-router.min.js';
+const VuexCDN = 'https://cdn.jsdelivr.net/npm/vuex@3.5.1/dist/vuex.min.js';
 const TinymceCDN = 'https://cdn.jsdelivr.net/npm/tinymce-all-in-one@4.9.3/tinymce.min.js';
 const html2canvasCDN = 'https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.7/dist/html2canvas.min.js';
 
